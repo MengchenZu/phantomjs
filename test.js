4195aac900d136f12d71cd2167fdab992a4b0233
root <root@debian>
2018-09-04 17:21:47 +1000

    Initialization

---
+++ test.js
@@ -0,0 +1,10 @@
+var page = require('webpage').create();
+
+url = 'file:///home/pilot/omnicl512/omniclient/public/slide_render/index.html?#signage'
+page.open(url, function(status) {
+  console.log('Status: ' + status);
+  setTimeout(function() {
+        page.render('google.png');
+        phantom.exit();
+  }, 10000);
+});
